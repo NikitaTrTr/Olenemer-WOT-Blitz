484f889fa07edbdd9b878a32bf84be46fd4edbc5
NikitaTrTr <tatnv01@gmail.com>
2023-07-21 15:57:32 +0300

    Refactor main

--- main.py
+++ main.py
@@ -1,68 +1,83 @@
+from PIL import ImageGrab
 from PyQt6 import QtWidgets
 from PyQt6 import QtCore
+from datetime import datetime
+
+from PyQt6.QtWidgets import QPushButton
+
 from gui import Ui_MainWindow
-from classes import player_stats_ui
-from statistic_checker import statistic_checker
+from classes import PlayerStatsUi
+from StatisticsChecker import StatisticsChecker
 import keyboard
-from pymouse import PyMouse
+
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
     def __init__(self, parent=None) -> None:
         super().__init__(parent)
+
+        self.config = "configs/1920.1080.125.txt"
+
         self.setupUi(self)
-        self.hook = keyboard.on_press(self.grab_scanning_event)
-        self.souz_team = self.generate_souz()
-        self.vragi_team = self.generate_vragi()
-        self.checker = statistic_checker()
+        self.al_team = self.generate_allies()
+        self.en_team = self.generate_enemies()
 
-        self.pushButton.setCheckable(True)
-        self.pushButton.clicked.connect(self.activate_scanning)
-
-        self.souz_stats.setStyleSheet("#souz_stats {background-color: rgba(74, 82, 113, 150); border: 1px solid darknavy;}")
-        self.vragi_stats.setStyleSheet("#vragi_stats {background-color: rgba(74, 82, 113, 150); border: 1px solid darknavy;}")
+        self.al_frame.setStyleSheet(
+            "#al_frame {background-color: rgba(74, 82, 113, 150); border: 1px solid darknavy; border-radius: 10px;}")
+        self.en_frame.setStyleSheet(
+            "#en_frame {background-color: rgba(74, 82, 113, 150); border: 1px solid darknavy; border-radius: 10px;}")
         self.setAttribute(QtCore.Qt.WidgetAttribute.WA_TranslucentBackground)
         self.setAttribute(QtCore.Qt.WidgetAttribute.WA_TransparentForMouseEvents)
-        self.setWindowFlags(QtCore.Qt.WindowType.FramelessWindowHint|QtCore.Qt.WindowType.WindowStaysOnTopHint)
+        self.setWindowFlags(QtCore.Qt.WindowType.FramelessWindowHint | QtCore.Qt.WindowType.WindowStaysOnTopHint)
+        self.hook = keyboard.on_press(self.on_key_press)
 
-    def grab_scanning_event(self, event):
+        self.button = QPushButton(self)
+        self.button.setCheckable(True)
+        self.button.clicked.connect(self.acivate_olenemer)
+        self.button.hide()
+
+    def on_key_press(self, event):
         if event.event_type == 'down':
             if event.scan_code == 87:
-                self.pushButton.click()
-            if event.scan_code == 67:
-                m = PyMouse()
-                m.click(980, 120, 1)
-    def activate_scanning(self):
-        self.fill_stats()
+                self.button.click()
+            if event.scan_code == 68:
+                if self.isHidden():
+                    self.show()
+                else:
+                    self.hide()
 
-    def fill_stats(self):
-        players = self.checker.return_olenei()
-        souz_team_stats, vragi_team_stats = players[0], players[1]
+    def acivate_olenemer(self):
+        image = ImageGrab.grab(bbox=(0, 0, 1920, 1080))
+        filename = datetime.now().strftime("image_%Y-%m-%d_%H-%M-%S.png")
+        image.save(f'screenshots/{filename}')
+        statistics = StatisticsChecker.get_players_statistics(self.config, f'screenshots/{filename}')
         for i in range(7):
-            self.fill_player_stats(self.souz_team, souz_team_stats[i], i)
+            self.al_team[i].set_stats(statistics['allies'][i])
+            self.en_team[i].set_stats(statistics['enemies'][i])
+
+    def generate_allies(self):
+        allies_team = []
+        self.al_frame.setLayout(self.al_panel)
         for i in range(7):
-            self.fill_player_stats(self.vragi_team, vragi_team_stats[i], i)
+            player_ui = PlayerStatsUi(self.layoutWidget, self.al_panel)
+            allies_team.append(player_ui)
+        return allies_team
 
-    def fill_player_stats(self, team, player_stats, index):
-        team[index].win_rate_field.setHtml(str(player_stats.win_rate))
-
-    def generate_souz(self):
-        souz_team = []
+    def generate_enemies(self):
+        enemies_team = []
+        self.en_frame.setLayout(self.en_panel)
         for i in range(7):
-            player = player_stats_ui("", "", "", self.souz_winrates, self.souz_tank, self.souz_tank_stats, self.layoutWidget)
-            souz_team.append(player)
-        return souz_team
+            player_ui = PlayerStatsUi(self.layoutWidget, self.en_panel)
+            enemies_team.append(player_ui)
+        return enemies_team
 
-    def generate_vragi(self):
-        vragi_team = []
-        for i in range(7):
-            player = player_stats_ui("", "", "", self.vragi_winrates, self.vragi_tank, self.vragi_tank_stats, self.layoutWidget)
-            vragi_team.append(player)
-        return vragi_team
 
-if __name__ == '__main__':
+def main():
     app = QtWidgets.QApplication([])
     window = MainWindow()
     window.showFullScreen()
-    #window.show()
     app.exec()
+
+
+if __name__ == '__main__':
+    main()
